9fb2093bc891f29de6545806a8f1ebf159b7f0b7
Josh Koter <josh.koter@gmail.com>
2026-01-07 17:43:22 +0200

    experimenting

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,70 @@
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import "./style.css";
 import * as THREE from "three";
 
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#000000");
+scene.background = new THREE.Color("#fff");
 
 // camera
-const aspectRatio = window.innerWidth / window.innerHeight;
+const getAspectRatio = () => window.innerWidth / window.innerHeight;
 const FOV = 75;
 const frustumNear = 0.1;
 const frustumFar = 1000;
 const camera = new THREE.PerspectiveCamera(
   FOV,
-  aspectRatio,
+  getAspectRatio(),
   frustumNear,
   frustumFar
 );
+// have to put the camera somwhere
 camera.position.z = 5;
 
 // create object
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.DodecahedronGeometry();
 const material = new THREE.MeshLambertMaterial({
-  color: "#468585",
-  emissive: "#468585"
+  color: "#ff0000",
+  emissive: "#0000ff"
 });
-// material = new THREE.MeshDepthMaterial();
-const cube = new THREE.Mesh(geometry, material);
+const dodecahedron = new THREE.Mesh(geometry, material);
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  color: "#ff0000",
+  emissive: "#00ff00"
+});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
 
 // add to scene
-scene.add(cube);
+scene.add(dodecahedron);
+scene.add(box);
 
 // lighting
-const light = new THREE.DirectionalLight(0x9cdba6, 10);
+const light = new THREE.SpotLight(0x99fbf1, 10);
 light.position.set(1, 1, 1);
 scene.add(light);
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
 document.body.appendChild(renderer.domElement);
+
+// add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
 
 // animate
 let iterator = 0; // using this for debugging
-let scaleDirection = 1;
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  dodecahedron.rotateX(0.01);
+  dodecahedron.rotateY(0.01);
 
-  cube.scale.x += 0.001 * scaleDirection;
-  cube.scale.y += 0.001 * scaleDirection;
-  cube.scale.z += 0.001 * scaleDirection;
-
-  if (cube.scale.x >= 1.5) scaleDirection = -1;
-  else if (cube.scale.x <= 1) scaleDirection = 1;
-
-  // cube.scale.x = cube.scale.x >= 2 ? cube.scale.x - 0.01 : cube.scale.x + 0.01;
+  controls.update();
 
   if (iterator === 30) {
     // console.log(cube);
@@ -67,3 +77,10 @@ function animate() {
 }
 
 animate();
+
+// handle window resizing
+window.addEventListener("resize", () => {
+  camera.aspect = getAspectRatio();
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
